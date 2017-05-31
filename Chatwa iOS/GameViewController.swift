782b011734737a05c15e00cc107ce27dc7aac608
Javon Davis <javonldavis14@gmail.com>
2017-05-31 15:28:06 -0500

    added launchscreen

---
+++ Chatwa iOS/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  Chatwa iOS
+//
+//  Created by Javon Davis on 31/05/2017.
+//  Copyright © 2017 Chatwa. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
